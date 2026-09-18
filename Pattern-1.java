5bebf60a7975b31a5d28e69254350d5ca9589453
Prashant-Sharma002 <prashantsharma3018@gmail.com>
2026-09-18 15:45:08 +0530

    Added Patterns folder

---
+++ Pattern-1.java
@@ -0,0 +1,12 @@
+class Pattern_1 {
+    public static void pattern1(int n){
+    //outter loop
+    for (int i=0; i<n;i++){
+       for (int j=0;j<n;j++){
+           System.out.print("*");
+       }
+        System.out.println();
+    }
+    return;
+}
+}
